2ca85bcccc552e2bfa24681f44dce7b97f93af38
QuangTrinh1612 <trinhquocquang.buh@gmail.com>
2025-02-19 21:40:25 +0700

    Added ConsumerDemo

--- src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -3,39 +3,36 @@ package io.conduktor.demos.kafka;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ProducerDemo {
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.info("I am a Kafka Producer");
+        log.info("Hello World ...");
 
-        String bootstrapServers = "127.0.0.1:9092";
-
-        // create Producer properties
+        // Create Producer Properties
         Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        
+        // Connect to localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
-        // create the producer
+        // Set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // Create Kafka Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // create a producer record
-        ProducerRecord<String, String> producerRecord =
-                new ProducerRecord<>("demo_java", "hello world");
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
 
-        // send data - asynchronous
         producer.send(producerRecord);
 
-        // flush data - synchronous
         producer.flush();
-        // flush and close producer
+
         producer.close();
     }
 }
