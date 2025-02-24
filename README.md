42db5808b52eb29fb4d8d62164a9c26c1d2c780c
QuangTrinh1612 <trinhquocquang.buh@gmail.com>
2025-02-24 20:07:38 +0700

    Added Graceful Shutdown in Consumer

---
+++ README.md
@@ -0,0 +1,19 @@
+# Kafka Producer
+## Producer Callback
+(To be updated)
+
+# Kafka Consumer
+## Graceful Shutdown
+(To be updated)
+
+# Kafka Consumer Group
+## Cooperative rebalance
+- Kafka Consumer: `partition.assignment.strategy`
+    - RangeAssignor
+    - RoundRobin
+    - StickyAssignor
+    - CooperativeStickyAssignor
+
+## Auto Offset Commit Behaviour
+- `auto.commit.interval.ms=5000`
+- `enable.auto.commit=true`
--- src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
+++ src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +20,7 @@ public class ConsumerDemo {
         log.info("I am a Kafka Consumer...");
 
         String groupId = "my-java-application";
-        String topic = "demo_java";
+        String topic = "java_topic";
 
         Properties properties = new Properties();
 
@@ -31,18 +32,46 @@ public class ConsumerDemo {
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
-        consumer.subscribe(Arrays.asList(topic));
+        // get reference to the mainThread
+        final Thread mainThread = Thread.currentThread();
 
-        while (true) {
-            log.info("Polling ...");
-            
-            ConsumerRecords<String, String> records = 
-                consumer.poll(Duration.ofMillis(1000));
+        // add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                log.info("Detect a shutdown, let's exit by calling consumer.wakeup()...");
+                consumer.wakeup();
 
-            for (ConsumerRecord<String, String> record: records) {
-                log.info("Key: " + record.key() + ", value: " + record.value());
-                log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                // join the main thread to allow execution of the code in the main thead
+                try {
+                    mainThread.join();    
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        });
+
+        try {
+            consumer.subscribe(Arrays.asList(topic));
+
+            while (true) {
+                log.info("Polling ...");
+                
+                ConsumerRecords<String, String> records = 
+                    consumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, String> record: records) {
+                    log.info("Key: " + record.key() + ", value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Consumer is starting to shut down");
+        } catch (Exception e) {
+            log.error("Unexpected exception in the consumer", e);
+        } finally {
+            consumer.close();
+            log.info("The consumer is now gracefully shut down...");
         }
     }
 }
